4b55217f3f04306f84ede15f8e5714742f91fe6e
Brian Cullinan <megamindbrian@gmail.com>
2026-04-09 17:33:18 -0700

    better packaging for plugins

--- RazorSharp/Extensions/RenderExtensions.cs
+++ RazorSharp/Extensions/RenderExtensions.cs
@@ -36,8 +36,6 @@ namespace RazorSharp.Extensions
 
 
 
-
-
         public static RenderFragment ToNavLink<TComponent>(
             Expression<Func<TComponent, TComponent>>? initializer = null
             , string? overrideTitle = null
@@ -46,27 +44,57 @@ namespace RazorSharp.Extensions
         {
             var values = initializer?.ToDictionary();
             var uri = TypeExtensions.GetUri<TComponent>(values);
-            var display = typeof(TComponent).GetCustomAttribute<DisplayAttribute>();
+            return ToNavLink<TComponent>(uri, overrideTitle, overrideIcon);
+        }
+
+        public static RenderFragment ToNavLink<TComponent>(
+            string uri
+            , string? overrideTitle = null
+            , string? overrideIcon = null)
+            where TComponent : class, IComponent
+        {
+            var type = typeof(TComponent);
+            return ToNavLink(uri, overrideTitle, overrideIcon, type);
+        }
+
+        public static RenderFragment ToNavLink(
+            string uri
+            , string? overrideTitle = null
+            , string? overrideIcon = null
+            , Type? type = null)
+        {
+            var display = type?.GetCustomAttribute<DisplayAttribute>();
+            return ToNavLink(uri, overrideTitle ?? display?.ShortName ?? display?.Name, overrideIcon ?? display?.Prompt);
+        }
+
+        public static RenderFragment ToNavLink(
+            string uri
+            , string? overrideTitle = null
+            , string? overrideIcon = null)
+        {
             return (__builder) =>
             {
                 __builder.OpenComponent<NavLink>(0);
                 __builder.AddAttribute(1, "class", "header-link");
                 __builder.AddAttribute(2, "href", uri);
-                __builder.AddAttribute(3, "Match", NavLinkMatch.All);
+                if(string.IsNullOrWhiteSpace(uri))
+                    __builder.AddAttribute(3, "Match", NavLinkMatch.All);
+                else
+                    __builder.AddAttribute(3, "Match", NavLinkMatch.Prefix);
 
                 // The inner HTML of a component is passed via the "ChildContent" attribute
                 __builder.AddAttribute(4, "ChildContent", (RenderFragment)((__builder2) =>
                 {
                     // 1. The <i> icon tag
                     __builder2.OpenElement(0, "i");
-                    __builder2.AddAttribute(1, "class", $"bi ${overrideIcon ?? display?.Prompt}");
+                    __builder2.AddAttribute(1, "class", $"bi ${overrideIcon}");
                     __builder2.AddAttribute(2, "aria-hidden", "true");
                     __builder2.CloseElement();
 
                     // 2. The <span> text tag
                     __builder2.OpenElement(3, "span");
                     __builder2.AddAttribute(4, "class", "header-text");
-                    __builder2.AddContent(5, overrideTitle ?? display?.Name);
+                    __builder2.AddContent(5, overrideTitle);
                     __builder2.CloseElement();
                 }));
 
